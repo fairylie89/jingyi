fa8d529deee1cddea6e48a8dbf9253fd9377dfca
jingyi <fairylie89@gmail.com>
2018-04-26 09:04:40 -0400

    Add touchStart for mobile

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-  $('#float').click(function(){
+  $('#float').on('touchstart click',(function(){
     $('#content').toggleClass("hide show"); 
     $('#toggle').toggleClass("fa-list-ul fa-times");
-  })
+  }))
 });
