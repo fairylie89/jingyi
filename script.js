cd04c2fd24774e498da615bf30f0eebb9ca39c5c
jingyi <fairylie89@gmail.com>
2018-04-26 17:32:07 -0400

    grammar

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
   $('#float').on('touchstart click',(function(){
     $('#content').toggleClass("hide show"); 
     $('#toggle').toggleClass("fa-list-ul fa-times");
-  }))
-  $('#content a').on('touchstart click',function(){
+  }));
+  $('#content a').on('touchstart click',(function(){
     $('#content').toggleClass("show hide"); 
     $('#toggle').toggleClass("fa-times fa-list-ul");
-  })
+  }));
 });
